b855639fb89df38040898c984cfb6d108499a15d
funhan <rnjstkdah2002@gmail.com>
2020-08-12 08:05:41 +0900

    Add files via upload
    ㅁㄴㅇ

---
+++ TCPClient.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class TCPClient : MonoBehaviour
+{
+    private TcpClient tcpClient;
+    private Thread tcpClientThread;
+
+    private string ipAddress = "127.0.0.1";
+    private const int port = 8888;
+
+    byte[] sizeBuffer;
+    byte[] typeBuffer;
+    byte[] bodyBuffer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tcpClientThread = new Thread(new ThreadStart(Listen));
+        tcpClientThread.IsBackground = true;
+        tcpClientThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SendMessage();
+        }
+    }
+
+    private void Listen()
+    {
+        tcpClient = new TcpClient(ipAddress, port);
+
+        byte[] bytes = new byte[1024];
+        while (true)
+        {
+            using (NetworkStream stream = tcpClient.GetStream())
+            {
+                int length;
+                while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+
+                    byte[] data = new byte[length];
+                    Array.Copy(bytes, 0, data, 0, length);
+
+                    string msg = Encoding.UTF8.GetString(data);
+                    Debug.Log("client msg : " + msg);
+                }
+            }
+        }
+    }
+
+    private void SendMessage()
+    {
+        if (tcpClient == null)
+        {
+            return;
+        }
+        NetworkStream stream = tcpClient.GetStream();
+        if (stream.CanWrite)
+        {
+            string clientMsg = "hello";
+            int size = 5;
+            byte[] s = BitConverter.GetBytes(size);
+            byte[] msg = Encoding.UTF8.GetBytes(clientMsg);
+            //stream.Write(msg, 0, msg.Length);
+
+            byte[] final = Combine(s, msg);
+            Debug.Log("string : " + BitConverter.ToString(final));
+
+            stream.Write(final, 0, final.Length);
+            Debug.Log("client sent message");
+        }
+    }
+
+    public static byte[] Combine(byte[] first, byte[] second)
+    {
+        byte[] ret = new byte[first.Length + second.Length];
+        Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+        Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+        return ret;
+    }
+}
+
---
+++ TCPServer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class TCPServer : MonoBehaviour
+{
+    private TcpListener tcpListener;
+    private Thread tcpListenerThread;
+    private TcpClient client;
+    
+    private string ipAddress = "127.0.0.1";
+    private const int port = 8888;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tcpListenerThread = new Thread(new ThreadStart(Listen));
+        tcpListenerThread.IsBackground = true;
+        tcpListenerThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Listen()
+    {
+        IPAddress ipAddr = IPAddress.Parse(ipAddress);
+        IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+        tcpListener = new TcpListener(ipEndPoint);
+        tcpListener.Start();
+        Debug.Log("Server is listening");
+
+        byte[] bytes = new byte[1];
+
+        const int headerSize = 4;
+        int offset = 0;
+        int lengthToRead = 0;
+        int size;
+        string body;
+
+        int check = 0;
+        byte[] buf = new byte[4];
+        
+
+        while (true)
+        {
+            using (client = tcpListener.AcceptTcpClient())
+            {
+                using (NetworkStream stream = client.GetStream())
+                {
+                    int length = 0;
+                    
+                    switch (check) {
+                        case 0:
+                            if (offset >= headerSize)
+                            {
+                                Debug.Log("asdasd");
+                                check = 1;
+                                byte[] data = new byte[headerSize];
+                                Array.Copy(bytes, 0, data, 0, headerSize);
+                                int ts = BitConverter.ToInt32(buf, 0);
+                                //string msg = Encoding.UTF8.GetString(data);
+                                Debug.Log("client msg : " + ts + " " + headerSize);
+                            }
+                            while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                            {
+                                if (offset + length <= headerSize)
+                                {
+                                    Array.Copy(bytes, 0, buf, offset, length);
+                                    offset += length;
+                                    string msg = Encoding.UTF8.GetString(bytes);
+                                    Debug.Log("client test : " + msg.ToString());
+                                    Debug.Log(offset);
+                                    //byte[] data = new byte[length];
+                                    //Array.Copy(bytes, 0, data, 0, length);
+                                }                                
+
+                                //string msg = Encoding.UTF8.GetString(data);
+                                //Debug.Log("client msg : " + msg + " " + length);
+                            }
+                            break;
+                        case 1:
+
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
